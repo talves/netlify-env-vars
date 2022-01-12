10bd5b61fa1e3b6419c3973632304e1acbd548fe
talves <tony@3alves.com>
2022-01-12 13:21:35 -0800

    show NODE_ENV

--- show-env.js
+++ show-env.js
@@ -1,6 +1,7 @@
 const build_env = `${process.env.BUILD_ENV}`;
+const node_env = `${process.env.NODE_ENV}`;
 console.log("====================================");
-console.log(`NODE_ENV: '${process.env.NODE_ENV}'`);
+console.log(`NODE_ENV: '${node_env}'`);
 console.log(`BUILD_ENV: '${build_env}'`);
 console.log("====================================");
 console.log(JSON.stringify(process.env, null, 2));
